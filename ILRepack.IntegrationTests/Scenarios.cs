c47d8c932fa679631bb9b5e130d4efaff6fbac0d
Matthew Leibowitz <mattleibow@live.com>
2023-03-30 11:47:26 +0200

    Update Scenarios.cs

--- ILRepack.IntegrationTests/Scenarios.cs
+++ ILRepack.IntegrationTests/Scenarios.cs
@@ -9,7 +9,7 @@ namespace ILRepack.IntegrationTests
     [Platform(Include = "win")]
     public class Scenarios
     {
-        private const int ScenarioProcessWaitTimeInMs = 15000;
+        private const int ScenarioProcessWaitTimeInMs = 30 * 1000;
 
         [Test]
         public void GivenXAMLThatUsesLibraryClass_MergedWPFApplicationRunsSuccessfully()
